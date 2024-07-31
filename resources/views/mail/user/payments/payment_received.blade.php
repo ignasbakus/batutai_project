33e9afce53d90d5537037e227106658711cf7d8e
ignasbakus <ignas.bakus77@gmail.com>
2024-07-31 11:19:50 +0300

    bug email 1.5

--- resources/views/mail/user/payments/payment_received.blade.php
+++ resources/views/mail/user/payments/payment_received.blade.php
@@ -114,12 +114,14 @@
     <div
         style="display: flex; flex-direction: column; align-items: center; justify-content: center; font-size: 12px; color: black; background-color: #B6D2F7; max-width: 650px; min-height: 150px; margin: 0 auto; padding: 20px; text-align: center;">
         <!-- Facebook Icon -->
-        <a href="ADDLINKTOFACEBOOK" style="margin-bottom: 10px; text-decoration: none; color: black;">
+        <a href="ADDLINKTOFACEBOOK" style="margin-bottom: 10px; text-decoration: none; color: black; font-family: 'Open Sans', sans-serif;" target="_blank">
+            <!-- Fallback text -->
             <svg style="margin-bottom: 10px;" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                <path
-                    d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"></path>
+                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"></path>
             </svg>
+            <span style="display: none;">Facebook</span>
         </a>
+
         <a href="{{config('app.link_to_homepage')}}" style="font-size: 16px;">op-op.lt</a>
         <a href="{{config('contactInfo.phone')}}"
            style="font-size: 18px; font-weight: 500; text-decoration: none; color: black;">
