19d265d46da95ef8acaae3b1cbd17f62ee65ec09
LeBrounius <ignas.bakus77@gmail.com>
2024-08-12 08:37:04 +0300

    changes made for MVP background 1.4

--- public/js/trampolines/public/trampolines_public.js
+++ public/js/trampolines/public/trampolines_public.js
@@ -22,8 +22,8 @@ let view = {
 
         } else if ($(window).width() > 1024 && $(window).width() <= 1421) {
             Trampolines.PcCarousel = true;
-            $('#carousel-wrap').attr('style', 'width: 35%; margin: 0 auto;');
-            $('.trampoline-name').show()
+            $('#carousel-wrap').attr('style', 'width: 40%; margin: 0 auto;');
+            // $('.trampoline-name').show()
 
             //telefonas
         } else if ($(window).width() < 768) {
